ba04f5c83d96014cdf9d8e6a4464827db83e2931
Zac Joffe <zacharyjoffe@gmail.com>
2019-12-01 00:07:54 -0500

    Convert byte vec to u32 vec

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,26 @@ fn main() {
         }
 
         println!("{:x?}", bytes);
+
+        let instrs: Vec<u32> = convert_bytes(bytes);
+
+        println!("{:08x?}", instrs)
     }
 }
 
+fn convert_bytes(bytes: Vec<u8>) -> Vec<u32> {
+    let mut instrs: Vec<u32> = Vec::new();
+
+    let mut i = 0;
+
+    while (i < bytes.len()) {
+        let instr: u32 = (((bytes[i] as u32) << 24) | ((bytes[i + 1] as u32) << 16) | ((bytes[i + 2] as u32) << 8) | bytes[i + 3] as u32) as u32;
+        instrs.push(instr);
+        i += 4;
+    }
+
+    instrs
+}
 
 fn disassemble() {
 
